a4ee2de0883dfee89ce99391e83978eb08736b5d
TanNguyen <mntan998@gmail.com>
2017-03-28 02:37:35 +0700

    Attaching onTouch event to button

--- C_LEDDEMO/ViewController.m
+++ C_LEDDEMO/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *buttonOnOff;
+@property (weak, nonatomic) IBOutlet UIImageView *ledPhoto;
 
 @end
 
@@ -16,13 +18,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _ledPhoto.image = [UIImage imageNamed:@"circle_blue"];
+    NSString *path = [[NSBundle mainBundle] resourcePath];
+    NSLog(@"%@", path);
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)onButtonTap:(id)sender {
+    if ([_ledPhoto.image isEqual: [UIImage imageNamed:@"circle_blue"]]) {
+        _ledPhoto.image = [UIImage imageNamed:@"circle_green"];
+    } else {
+        _ledPhoto.image = [UIImage imageNamed:@"circle_blue"];
+    }
 }
 
 
